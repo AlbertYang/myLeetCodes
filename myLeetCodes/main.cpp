4a8aca983c62a40dcd8d0e659a6cc6ad98ac143a
Albert <yzt0806@gmail.com>
2016-02-25 20:54:06 +0800

    Initial Commit

---
+++ myLeetCodes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myLeetCodes
+//
+//  Created by 杨照通 on 2/25/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
